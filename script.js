4d08591c8bbf9dade5e88765b38ecfdc12ef45d6
allanheil1 <allanheil1@gmail.com>
2022-10-28 16:51:35 -0300

    Recarrega página ao enviar mensagem

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let username;
-const APIaddress = "https://mock-api.bootcamp.respondeai.com.br/api/v6/uol";
+const APIaddress = 'https://mock-api.bootcamp.respondeai.com.br/api/v6/uol';
 let destino = 'Todos';
 let messageType = 'message';
 
-function entrarNaSala(){
-    username = prompt("Qual é a sua graça?");
+function entraNaSala(){
+    username = prompt('Escolha seu nome de usuário (username)');
     const promise = axios.post(`${APIaddress}/participants`,  { name: username });
     promise.then(puxaMensagens);
     promise.catch(erroAoEntrar);
@@ -25,17 +25,16 @@ function erroAoEntrar(){
 
 function renderizaMensagens(response){
     console.log(response.status);
-    let htmlDasMensagens = document.querySelector(".chat-container");
+    let htmlDasMensagens = document.querySelector('.chat-container');
     htmlDasMensagens.innerHTML = '';
-
     for(let i = 0; i < response.data.length; i++){
         console.log(response.data[i]);
         const message = response.data[i];    
   
         if(message.type === 'private_message' && (message.from === username || message.to === username)){
             htmlDasMensagens.innerHTML += `
-            <li class = "private-message">
-                <span class = "hora">${message.time}</span>
+            <li class = 'private-message'>
+                <span class = 'hora'>${message.time}</span>
                 <strong> ${message.from} </strong>
                 <span> para <span>
                 <strong> ${message.to}: </strong>
@@ -46,8 +45,8 @@ function renderizaMensagens(response){
 
         if(message.type === 'status'){
             htmlDasMensagens.innerHTML += `
-            <li class = "status-message">
-                <span class = "hora">${message.time}</span>
+            <li class = 'status-message'>
+                <span class = 'hora'>${message.time}</span>
                 <strong> ${message.from} </strong>
                 <span> para <span>
                 <strong> ${message.to}: </strong>
@@ -58,8 +57,8 @@ function renderizaMensagens(response){
 
         if(message.type === 'message'){
             htmlDasMensagens.innerHTML += `
-            <li class = "public-message">
-                <span class = "hora">${message.time}</span>
+            <li class = 'public-message'>
+                <span class = 'hora'>${message.time}</span>
                 <strong> ${message.from} </strong>
                 <span> para <span>
                 <strong> ${message.to}: </strong>
@@ -68,15 +67,29 @@ function renderizaMensagens(response){
             `
         }
     }
+    scrollIntoLast();
+}
+
+function scrollIntoLast(){
+    //scrollamos a página até a última mensagem
+    const lastMsg = document.querySelector('.chat-container').lastChild;
+    lastMsg.scrollIntoView();
 }
 
 function enviarMensagem(){
     //seleciona o input
     const messageToSend = document.querySelector('input').value;
     //envia para a api
-    axios.post(`${APIaddress}/messages`, {from: username, to: destino, text: messageToSend, type: messageType});
+    const messageDone = axios.post(`${APIaddress}/messages`, {from: username, to: destino, text: messageToSend, type: messageType});
+    messageDone.then(enviouMensagem);
+
+}
+
+function enviouMensagem(){
     //limpa o campo de input de mensagem
     document.querySelector('input').value = '';
+    //após enviar a mensagem, vamos renderizar as mensagens na tela imediatamente
+    puxaMensagens();
 }
 
 function mantemLogado(){
@@ -87,10 +100,10 @@ function mantemLogado(){
 
 //funcao que mantém gerencia as principais funcionalidades do chat
 function gerenciaChat(){
-    entrarNaSala();
+    entraNaSala();
 
     setInterval(mantemLogado, 5000);
-    setInterval(puxaMensagens, 2000);
+    setInterval(puxaMensagens, 3000);
 }
 
 gerenciaChat();
